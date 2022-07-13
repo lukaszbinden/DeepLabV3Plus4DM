b5411e5438d60a1affbc04e9ac9e4c9610c7953c
Zbinden Lukas (lz20w714) <lz20w714@submit01.ubelix.unibe.ch>
2022-07-13 13:31:10 +0200

    add script

---
+++ continue_train.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#SBATCH --job-name="DLv3+512 conttr"
+#SBATCH --partition=gpu-invest
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=23:59:59
+#SBATCH --mem-per-cpu=15G
+#SBATCH --gres=gpu:rtx3090:1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=logs/512_train_cont.txt
+#SBATCH --error=logs/512_train_cont.txt
+#SBATCH --account=ws_00000
+
+export NOW=$( date '+%F-%H-%M-%S' )
+
+
+echo "Copying training data to ${TMPDIR}..."
+mkdir $TMPDIR/cityscapes_toy
+cp /storage/workspaces/artorg_aimi/ws_00000/lukaszbinden/datasets/cityscapes_toy.tar.gz $TMPDIR/cityscapes_toy
+## /storage/homefs/lz20w714/aimi_storage/lukaszbinden/datasets
+## (ddpm) [lz20w714@submit02 datasets]$ tar -cvf cityscapes_toy.tar.gz cityscapes_toy/
+tar -xf $TMPDIR/cityscapes_toy/cityscapes_toy.tar.gz -C $TMPDIR/.
+
+python main.py --model deeplabv3plus_mobilenet --dataset cityscapes --gpu_id 0  --lr 0.1  --crop_size 256 --batch_size 48 --output_stride 16 --data_root ${TMPDIR}/cityscapes_toy/ --print_interval 500 --val_interval 5000 --total_itrs 300000 --ckpt checkpoints/latest_deeplabv3plus_mobilenet_cityscapes_os16.pth --save_val_results --val_max_size 500 --continue_training
+
+
